c1c4f73fbe61950e559c6204a2d7e2d20b5f151f
Nina Okumura <elaineokumura@gmail.com>
2022-08-26 17:54:50 +1200

    add assets + custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "inear-gradient": "hsl(249, 99%, 64%) to hsl(278, 94%, 30%)",
+        red: "hsl(0, 100%, 66%)",
+        white: "hsl(0, 0%, 100%)",
+        "light-grayish-violet": "hsl(270, 3%, 87%)",
+        "dark-grayish-violet": "hsl(279, 6%, 55%)",
+        "very-dark-violet": "hsl(278, 68%, 11%)",
+      },
+      fontFamily: {
+        "sans-serif": ["Space Grotesk", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
