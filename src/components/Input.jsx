e905956846856c83d1c9d2f2d1db2fce73a0fca4
Nina Okumura <elaineokumura@gmail.com>
2022-09-02 18:36:45 +1200

    wrap input with label

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,16 +5,14 @@ export default function Input(props) {
     <div className="flex flex-col gap-2">
       <label className="font-semibold tracking-[0.1em] font-sans-serif w-full text-xs">
         {props.label}
+        <input
+          type={props.type}
+          value={props.value}
+          onChange={props.onChange}
+          placeholder={props.placeholder}
+          className="border h-12 rounded-lg text-left w-full px-2"
+        />
       </label>
-      <input
-        type={props.type}
-        name={props.name}
-        value={props.value}
-        onChange={props.onChange}
-        placeholder={props.placeholder}
-        id={props.id}
-        className="border h-12 rounded-lg text-left w-full px-2"
-      />
     </div>
   );
 }
